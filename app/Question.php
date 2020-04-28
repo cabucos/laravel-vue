13e9be298025641a49a138c784f779f132942781
christian martin cabucos <christianmartincabucos@gmail.com>
2020-04-28 15:38:18 +0800

    Add votes, answer and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -9,21 +9,35 @@ class Question extends Model
 {
     protected $fillable = ['title','body'];
 
-    public function setTitleAttributes($value)
-    {
+    public function setTitleAttributes($value){
+
         return $this->attributes['title']  = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
 
     public function user(){
+
         return $this->belongsTo(User::class);
     }
 
     public function getUrlAttribute(){
+
         return route('question.show', $this->id);
     }
 
     public function getCreatedDateAttribute(){
+
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute(){
+
+        if($this->answers > 0){
+            if($this->best_answer_id){
+                return "answer-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
     }
 }
