99777dbb828db4cd7c4ee8ba81168776cb0cc2af
christian martin cabucos <christianmartincabucos@gmail.com>
2020-04-29 10:07:25 +0800

    Extedn form functionality to be able to update question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -30,23 +30,28 @@
                                 {{ $question->views . " " . str_plural('view', $question->views)}}
                             </div>
                         </div>
-                            <div class="media-body">
-                                <h3 class="mt-0"><a href=" {{ $question->url }}">{{ $question->title }}</a></h3>
-                                <p class="lead">
-                                    Asked 🇧🇾
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    <small class="text-muted">{{ $question->created_date}}</small>
-                                </p>
-                                {{ str_limit($question->body, 250) }}
+                        <div class="media-body">
+                            <div class="d-flex align-items-center">
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <div class="ml-auto">
+                                    <a href="{{ route('question.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                </div>
                             </div>
+                            <p class="lead">
+                                Asked 🇧🇾
+                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                <small class="text-muted">{{ $question->created_date}}</small>
+                            </p>
+                            {{ str_limit($question->body, 250) }}
                         </div>
-                        @endforeach
-                        <div class="mx-auto">
-                            {{ $questions->links() }}
-                        </div>
+                    </div>
+                    @endforeach
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
